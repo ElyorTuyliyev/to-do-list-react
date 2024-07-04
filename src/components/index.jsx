a1cc11ffd807738732c0cb89a774823c18b5bacf
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-07-04 16:47:51 +0500

    add todo list

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,17 +1,41 @@
 import "./style.css";
+import { useState } from "react";
 
 function TodoList() {
+  const [toDo, setToDo] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { text } = e.target;
+
+    setToDo([...toDo, text.value]);
+
+    e.target.reset();
+  };
+
   return (
-    <form className="form__wrapper">
-      <input
-        className="form__inp"
-        type="text"
-        placeholder="Enter your text"
-        name=""
-        id="todo"
-      />
-      <button className="form__btn">add</button>
-    </form>
+    <div>
+      <h2 className="toDo__title">Do List</h2>
+      <form className="form__wrapper" onSubmit={handleSubmit}>
+        <input
+          className="form__inp"
+          type="text"
+          placeholder="Enter your text"
+          name="text"
+          id="next"
+        />
+
+        <button className="form__btn">add</button>
+      </form>
+      <ul className="list">
+        {toDo.map((item, index) => (
+          <li className="item" key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
